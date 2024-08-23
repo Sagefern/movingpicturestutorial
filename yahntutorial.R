c495f2ba4e259663c074285eb3af4a24c4190ddd
Charlotte Lee <contact.charleez@gmail.com>
2024-08-23 15:25:25 +0800

    download necessary packages

---
+++ yahntutorial.R
@@ -0,0 +1,13 @@
+p1 <- c("tidyverse", "vegan", "BiocManager")
+p2 <- c("phyloseq", "ANCOMBC", "DESeq2", "ComplexHeatmap")
+load_package <- function(p) {
+  if (!requireNamespace(p, quietly = TRUE)) {
+    ifelse(p %in% p1, 
+           install.packages(p, repos = "http://cran.us.r-project.org/"), 
+           BiocManager::install(p))
+  }
+  library(p, character.only = TRUE, quietly = TRUE)
+}
+invisible(lapply(c(p1,p2), load_package))
+
+##Building a phyloseq object
